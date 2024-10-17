a3b0efb0b55af7366a6957319434df7039bf47b9
Ali Ahmed <ali@gmail.com>
2024-10-17 09:59:27 +0500

    POST API CREATED

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
+import {FormsModule} from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
+import { error } from 'console';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
-}
+  http  = inject(HttpClient);
+
+  data :any [] =[];
+
+  userData: any=
+  {
+    name : '',
+    email : '',
+    phone : '',
+    hospitalName : '',
+    clinicName : '',
+    location : '',
+  }
+
+  saveData(){
+    this.data = this.userData;
+    console.log(this.data);
+
+    this.http.post("", this.data).subscribe((res)=>{
+      console.log("User Registered Successfully");
+    });
+
+    this.userData = "";
+  }
+
+  }
